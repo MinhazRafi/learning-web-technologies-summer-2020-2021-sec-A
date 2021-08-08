509cfc8ac64c32626ce6a6f718aa1e8cc0d60b37
Minhazur Rahman <iamrafi404@gmail.com>
2021-08-08 00:21:59 -0400

    1st

---
+++ WT_LAB_FINAL/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<center>
+		Login
+		<form method="post" action="">
+			<table>
+
+				<tr>
+					<td>Username:</td>
+					<td><input type="text" name="Name" value=""></td>
+				</tr>
+
+				<tr>
+					<td>Password:</td>
+					<td><input type="password" name="password" value=""></td>
+				</tr>
+
+				<tr>
+					<td></td>
+					<td><input type="submit" name="submit" value="Login"></td>
+				</tr>
+
+			</table>
+		</form>
+	</center>
+</body>
+</html>
---
+++ WT_LAB_FINAL/reg.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<center>
+		Registration
+		<form method="post" action="reg.php">
+			<table>
+				<tr>
+					<td>Employer Name:</td>
+					<td><input type="text" name="Name" value=""></td>
+				</tr>
+
+				<tr>
+					<td>Company Name:</td>
+					<td><input type="text" name="Name" value=""></td>
+				</tr>
+
+				<tr>
+					<td>Contact No:</td>
+					<td><input type="number" name="number" value=""></td>
+				</tr>
+
+				<tr>
+					<td>Username:</td>
+					<td><input type="text" name="Name" value=""></td>
+				</tr>
+
+				<tr>
+					<td>Password:</td>
+					<td><input type="password" name="password" value=""></td>
+				</tr>
+
+				<tr>
+					<td></td>
+					<td><input type="submit" name="submit" value="Submit"></td>
+				</tr>
+
+			</table>
+		</form>
+	</center>
+</body>
+</html>
